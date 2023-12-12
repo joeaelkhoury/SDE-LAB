5e14cb87e5724e2012d06b697586060725e681d4
Joe El Khoury <101676215+joeaelkhoury@users.noreply.github.com>
2023-12-12 09:22:26 +0100

    Add files via upload

--- SDE_LAB3-main/app.js
+++ SDE_LAB3-main/app.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 const PORT = 5600;
 
-// Serve static files
+// Middleware for serving static files
 app.use(express.static(path.join(__dirname, '.')));
+app.use(express.json());  // Middleware for parsing JSON payloads
 
-app.get('/fetchJoke', async (req, res) => {
+// Endpoint to get a joke
+app.get('/getJoke', async (req, res) => {
     const apiUrl = "https://humor-jokes-and-memes.p.rapidapi.com/jokes/random";
-    const prompt = req.query.prompt || ''; 
+    const prompt = req.query.prompt || '';  // Capture the keyword
 
     try {
         const response = await axios.get(apiUrl, {
@@ -33,15 +35,59 @@ app.get('/fetchJoke', async (req, res) => {
     }
 });
 
-app.get('/script.js', (req, res) => {
+// Endpoint to translate a joke
+app.post('/translateJoke', async (req, res) => {
+    const text = req.body.text;
+
+    try {
+        const response = await axios.post('https://microsoft-translator-text.p.rapidapi.com/translate?to[0]=it&api-version=3.0&from=en&profanityAction=NoAction&textType=plain', 
+        [{ Text: text }], 
+        {
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': 'ac6ff202a0msha62eaa2244cd01bp14eb56jsnc98f9268668c',
+                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+            }
+        });
+
+        if (response && response.data && Array.isArray(response.data) && response.data.length > 0 && response.data[0].translations && response.data[0].translations.length > 0) {
+            const translatedText = response.data[0].translations[0].text;
+            res.json({ translatedText });
+        } else {
+            res.status(500).json({ error: 'Unexpected API response' });
+        }
+
+    } catch (error) {
+        console.error('Error translating joke:', error);
+        res.status(500).json({ error: 'Failed to translate joke' });
+    }
+});
+
+// Serving the fetchJokeAndTranslate function as a separate JavaScript script
+app.get('/app2.js', (req, res) => {
     res.type('text/javascript');
     res.send(`
-        async function fetchJoke() {
-            const prompt = document.getElementById('jokePrompt').value;
+        async function fetchJokeAndTranslate() {
             try {
-                const response = await fetch('/fetchJoke?prompt=' + encodeURIComponent(prompt));
-                const data = await response.json();
-                alert(data.joke);
+                const jokeResponse = await fetch('/getJoke');
+                const jokeData = await jokeResponse.json();
+                const randomJoke = jokeData.joke;
+
+                const translationResponse = await fetch('/translateJoke', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ text: randomJoke })
+                });
+                
+                const translationData = await translationResponse.json();
+
+                if (translationData.translatedText) {
+                    alert(translationData.translatedText);
+                } else {
+                    console.error('Unexpected translation response:', translationData);
+                }
             } catch (error) {
                 console.error('Error:', error);
             }
